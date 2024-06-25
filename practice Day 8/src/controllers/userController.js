701cb894b8f8b5c1f12481cbb2aa22ffbc46f7a1
haseeb hassan <its.haseebhassan07@gmail.com>
2024-06-25 13:04:33 +0500

    token created

--- practice Day 8/src/controllers/userController.js
+++ practice Day 8/src/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import createHttpError from "http-errors";
+import bcrypt from 'bcrypt'
+import Jwt from 'jsonwebtoken'
 const createUser=async(req,res,next )=>{
     const {name,email,password}=req.body
 // validation
@@ -25,5 +27,26 @@ const createUser=async(req,res,next )=>{
         const err=createHttpError(500,'Intrnal server error')
         return next(res.json(err))
     }
+    // hash password
+    const hashpassword=await bcrypt.hash(password,10)
+    // create user
+    let newUser;
+    try {
+        newUser=await User.create({name,email,password:hashpassword})
+    } catch (error) {
+        const err=createHttpError(500,'Intrnal server error')
+        return next(res.json(err))
+    }
+
+    // generate token
+    try {
+        const token=Jwt.sign({id:newUser._id},process.env.JWT_SECRET,{expiresIn:'1d'},{algorithm:'HS256'})
+       res.status(201).json({accessToken:token})    
+    } 
     
+    catch (error) {
+        const err=createHttpError(500,"token generation failed")
+        return next(res.json(err))
+    }
 }
+export {createUser}
